302e8b5134fb0c97ec314fce5406f4a632df9b67
mahmoudnourdein <mahmoudnourdein@gmail.com>
2020-09-30 21:35:40 +0300

    Step_6.html

---
+++ javascript-basics-01/step6.js
@@ -0,0 +1,8 @@
+
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("first_number").value;
+        num2 = document.getElementById("second_number").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
